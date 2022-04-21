d016df6de3aa239f02bbee8bd380120cc40fbab0
Eduardo Valencia <deep-e@hotmail.com>
2022-04-21 10:29:37 -0600

    short fix way for apikey error

--- src/Helpers/Axios/AxiosInstance.tsx
+++ src/Helpers/Axios/AxiosInstance.tsx
@@ -1,28 +1,6 @@
 import axios from 'axios';
 import {store} from '../../store/store';
 
-function axiosListener() {
-  const LOGIN_TOKEN = store.getState().Login;
-  let moviedbToken =
-    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYWE2NjMwMGU4NGZhZGJhMmUxYmNlMzhlYTBhM2I5YiIsInN1YiI6IjYyNWUyZmY0MTk2OTBjMDA1MWNjOTY3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BsprNV2vyx0SB6KhREjHsZUwUN05UuiU8ZPTu7fedMY';
-  if (LOGIN_TOKEN.token != undefined && LOGIN_TOKEN.token != '') {
-    axiosInstance.defaults.headers.common[
-      'Authorization'
-    ] = `Bearer ${moviedbToken}`;
-
-    axiosMoviesInstance.defaults.headers.common[
-      'Authorization'
-    ] = `Bearer ${moviedbToken}`;
-  } else {
-    delete axiosInstance.defaults.headers.common['Authorization'];
-  }
-}
-
-/**
- * Se subscribe al store para obtener el token automaticamente
- */
-store.subscribe(axiosListener);
-
 const URL_LOGIN_BASE = 'https://reqres.in/api/login';
 const URL_MOVIES_BASE = 'https://api.themoviedb.org/3';
 
@@ -43,3 +21,14 @@ export const axiosMoviesInstance = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+const LOGIN_TOKEN = store.getState().Login;
+let moviedbToken =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYWE2NjMwMGU4NGZhZGJhMmUxYmNlMzhlYTBhM2I5YiIsInN1YiI6IjYyNWUyZmY0MTk2OTBjMDA1MWNjOTY3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BsprNV2vyx0SB6KhREjHsZUwUN05UuiU8ZPTu7fedMY';
+axiosInstance.defaults.headers.common[
+  'Authorization'
+] = `Bearer ${moviedbToken}`;
+
+axiosMoviesInstance.defaults.headers.common[
+  'Authorization'
+] = `Bearer ${moviedbToken}`;
